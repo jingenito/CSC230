db9570ddc228f2e0af88c2071e18233c2fabd99f
Malekith <jingenito@vcssoftware.com>
2020-02-03 13:54:21 -0500

    work done in class 2-3-20

---
+++ lab1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    cin >> x;
+    cin >> y;
+
+    char arr[x][y];
+    for(int i = 0; i < x; ++i)
+        for(int j = 0; j < y; ++j)
+            cin >> arr[i][j];
+    
+    for(int i = 0; i < x; ++i)
+    {
+        for(int j = 0; j < y; ++j)
+        {
+            cout << arr[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+}
